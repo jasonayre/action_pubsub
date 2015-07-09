95cd5fdaf3046b7acdaad8e11eca784a1223da9e
Jason Ayre <jasonayre@gmail.com>
2015-07-09 15:37:47 -0600

    register exchange even if publisher hasnt defined it yet

--- lib/action_pubsub/exchange_registry.rb
+++ lib/action_pubsub/exchange_registry.rb
@@ -1,6 +1,7 @@
 module ActionPubsub
   class ExchangeRegistry < ::Concurrent::LazyRegister
     def register_queue(exchange_name, subscriber_name)
+      register_exchange(exchange_name) unless key?(exchange_name)
       exchange_hash = self[exchange_name].instance_variable_get("@data").value
       exchange_keys = exchange_hash.keys
       queue_name = [exchange_name, subscriber_name].join("/")
