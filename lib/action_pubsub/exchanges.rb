4f129499140fa4c004364455903bc627fc1c342a
Jason Ayre <jayre@peer60.com>
2016-01-04 15:25:24 -0700

    fix race condition when observing multiple attributes for changes

--- lib/action_pubsub/exchanges.rb
+++ lib/action_pubsub/exchanges.rb
@@ -2,7 +2,8 @@ module ActionPubsub
   class Exchanges < ::ActionPubsub::Registry
     def register_queue(exchange_name, subscriber_name)
       queue_name = [exchange_name, subscriber_name].join("/")
-      self[exchange_name].add(subscriber_name) { ::ActionPubsub::Queue.spawn(queue_name) }
+      queue_exists = self[exchange_name].all.any?{ |queue| queue.name == queue_name }
+      self[exchange_name].add(subscriber_name) { ::ActionPubsub::Queue.spawn(queue_name) } unless queue_exists
     end
 
     def register_exchange(exchange_name)
